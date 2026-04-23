18f4817d49dba6b40cc79079f515d4fefda6cc77
Akankshakundu <akankshakundu07@gmail.com>
2026-04-23 22:30:05 +0530

    Create code.v

---
+++ Logic-Gates/code.v
@@ -0,0 +1,13 @@
+module logic_gates(
+    input A, B,
+    output AND_out, OR_out, NAND_out, NOR_out, XOR_out, XNOR_out
+);
+
+assign AND_out  = A & B;
+assign OR_out   = A | B;
+assign NAND_out = ~(A & B);
+assign NOR_out  = ~(A | B);
+assign XOR_out  = A ^ B;
+assign XNOR_out = ~(A ^ B);
+
+endmodule
