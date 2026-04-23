1969a1fe44f5bb4c8f568a4035049b2479e7eb60
Akankshakundu <akankshakundu07@gmail.com>
2026-04-23 23:36:16 +0530

    Update code.v

--- COUNTERS/code.v
+++ COUNTERS/code.v
@@ -1,1 +1,13 @@
+module ring_counter(input CLK, output reg [3:0] Q = 4'b0001);
 
+always @(posedge CLK)
+    Q <= {Q[2:0], Q[3]};
+
+endmodule
+
+module johnson_counter(input CLK, output reg [3:0] Q = 4'b0000);
+
+always @(posedge CLK)
+    Q <= {~Q[0], Q[3:1]};
+
+endmodule
